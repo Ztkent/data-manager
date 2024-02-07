07df4f86e18eb77fedac5e1718f813005618e412
Zachary Kent <ztkent@gmail.com>
2024-02-07 15:36:37 -0500

    support some new endpoints

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -18,34 +20,58 @@ func main() {
 	})
 
 	r.Get("/network", func(w http.ResponseWriter, r *http.Request) {
+		// if html/network.html is not found, return some blank page
+		if _, err := os.Stat("html/network.html"); os.IsNotExist(err) {
+			startProcessor()
+			time.Sleep(1 * time.Second)
+		}
 		http.ServeFile(w, r, "html/network.html")
 	})
 
-	// startCrawlerWithOptions("pkg/data-crawler/crawl.json")
-	// startProcessor()
+	r.Post("/crawl", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Crawling started"))
+		startCrawlerWithOptions("pkg/data-crawler/crawl.json")
+	})
+
+	r.Get("/export", func(w http.ResponseWriter, r *http.Request) {
+		filePath := "pkg/data-crawler/results.db"
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			http.Error(w, "File not found", http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Disposition", "attachment; filename=results.db")
+		w.Header().Set("Content-Type", "application/octet-stream")
+		http.ServeFile(w, r, filePath)
+	})
 	http.ListenAndServe(":8080", r)
 }
 
 func startCrawlerWithOptions(path string) {
-	cmd := exec.Command("./pkg/data-crawler/v0.1.0/data-crawler", "-c", path)
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		cmd := exec.Command("./pkg/data-crawler/v0.1.0/data-crawler", "-c", path)
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
 
 func startCrawler() {
-	cmd := exec.Command("./pkg/data-crawler/v0.1.0/data-crawler")
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		cmd := exec.Command("./pkg/data-crawler/v0.1.0/data-crawler")
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
 
 func startProcessor() {
-	cmd := exec.Command("python3", "pkg/data-processor/data_processor.py", "--database", "pkg/data-crawler/results.db")
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		cmd := exec.Command("python3", "pkg/data-processor/data_processor.py", "--database", "pkg/data-crawler/results.db")
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 }
